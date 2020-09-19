68c85e5b05d699549bbd1c2fd5e93e20041c9270
komalshres <komalshres@gmail.com>
2020-09-19 15:39:20 +0545

    ui commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace TBSTech.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Members()
         {
             return View();
         }
